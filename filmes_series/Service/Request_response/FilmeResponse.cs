44329725d62408af2d33a8104c79b9ce643b3af4
André Bessa <andrebessax@gmail.com>
2022-10-07 22:41:26 -0300

    Update: Ajustes.

--- filmes_series/Service/Request_response/FilmeResponse.cs
+++ filmes_series/Service/Request_response/FilmeResponse.cs
@@ -5,11 +5,9 @@ namespace filmes_series.Service.Request_response
     public class FilmeResponse : ProducaoRequestResponse
     {
         public FilmeResponse()
-        {
-            
-            this.ElencoFilme = new List<AtorResponse>();
+        {            
+          this.ElencoFilme = new List<AtorResponse>();
         }
-
         public virtual CategoriaResponse Categoria { get; set; }
         public virtual ICollection<AtorResponse> ElencoFilme { get; set; }
     }
